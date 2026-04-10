8662ed07aa47ebf66f912341a4e36c888a6422c8
Tony Winn <hi@tonywinn.me>
2026-04-10 14:54:40 -0400

    Lower Elixir requirement to ~> 1.15, add CI test matrix

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Stagehand.MixProject do
     [
       app: :stagehand,
       version: @version,
-      elixir: "~> 1.19",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
